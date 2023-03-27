c1b6d40cbd6af7304bcb6c92bbe195f46d683e78
Martina Vangelova <mvangelovaa@abv.bg>
2023-03-27 09:24:51 +0300

    fixed header and nav bar with css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,25 @@
 import { Link } from "react-router-dom";
 
 
-export default function Header(){
-    
-    return (
-      <header>
+export default function Header() {
+
+  return (
+    <header>
       <nav>
         <div className="logo">
           <img src="images/main-logo.png" alt="logo" />
         </div>
         <ul className="nav-links">
-          <li><Link to="/">Home </Link></li>
-          <li><Link to="/catalog">Catalog </Link></li>
-          <li><Link to="/create">Create Cake </Link></li>
-          <li><Link to="/login">Login </Link></li>
-          <li><Link to="/register">Register</Link></li>
-          <li><Link to="/logout">Logout</Link></li>
+          <li>
+            <Link to="/">Home </Link>
+            <Link to="/catalog">Catalog </Link>
+            <Link to="/create">Create Cake </Link>
+            <Link to="/login">Login </Link>
+            <Link to="/register">Register</Link>
+            <Link to="/logout">Logout</Link>
+          </li>
         </ul>
       </nav>
     </header>
-    )
+  )
 }
