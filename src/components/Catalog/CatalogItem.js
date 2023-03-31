19378590e6aae426dc36f829f6c4a8e466cc0787
Martina Vangelova <mvangelovaa@abv.bg>
2023-03-31 17:22:40 +0300

    add like button

--- src/components/Catalog/CatalogItem.js
+++ src/components/Catalog/CatalogItem.js
@@ -15,12 +15,11 @@ export default function CatalogItem({
 
         <div className="pix_btn">
           <Link to={`/catalog/${_id}`} className="btn-hvr-effects mt-3">Details</Link>
-          <span className="like-text">Like</span>
         </div>
-        <button className="like-button">
-          <span className="heart-icon" src="images/icon-heart.png"></span>
-          <span className="like-text">Like</span>
-        </button>
+        <div className="">
+<span className = "heart colored">&#10084;</span>
+  </div>
+
       </div>
     </div>
   )
